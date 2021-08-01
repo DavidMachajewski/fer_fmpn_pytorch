fixed softmax bug

lib/agents/densenet_agent.py
@@ -164,16 +164,11 @@ class DenseNetAgent(Agent):
 
             self.opt.zero_grad()
 
-            # clsf = self.model(images).logits
             clsf = self.model(images)
-            # print(clsf)
-            cls_soft = torch.softmax(
-                clsf,
-                dim=-1
-            )
-            # print(cls_soft)
 
-            loss = self.loss_fn(cls_soft, labels)
+            cls_soft = torch.softmax(clsf, dim=-1)
+
+            loss = self.loss_fn(clsf, labels)
 
             loss.backward()
             self.opt.step()
@@ -195,12 +190,11 @@ class DenseNetAgent(Agent):
                 images = batch["image"].to(self.device)
                 labels = batch["label"].to(self.device)
 
-                cls_soft = torch.softmax(
-                    self.model(images),
-                    dim=-1
-                )
+                clsf = self.model(images)
 
-                loss = self.loss_fn(cls_soft, labels)
+                cls_soft = torch.softmax(clsf, dim=-1)
+
+                loss = self.loss_fn(clsf, labels)
 
                 epoch_val_loss += loss
                 epoch_val_acc += self.__calc_accuracy__(cls_soft, labels)
@@ -224,12 +218,11 @@ class DenseNetAgent(Agent):
                 images = batch["image"].to(self.device)
                 labels = batch["label"].to(self.device)
 
-                cls_soft = torch.softmax(
-                    self.model(images),
-                    dim=-1
-                )
+                clsf = self.model(images)
 
-                loss = self.loss_fn(cls_soft, labels)
+                cls_soft = torch.softmax(clsf, dim=-1)
+
+                loss = self.loss_fn(clsf, labels)
                 epoch_val_loss += loss
                 epoch_val_acc += self.__calc_accuracy__(cls_soft, labels)
 

lib/agents/resnet_agent.py
@@ -160,12 +160,11 @@ class ResNetAgent(Agent):
 
             self.opt.zero_grad()
 
-            # clsf = self.model(images).logits
             clsf = self.model(images)
-            # print(clsf)
+
             cls_soft = torch.softmax(clsf, dim=-1)
-            # print(cls_soft)
-            loss = self.loss_fn(cls_soft, labels)
+
+            loss = self.loss_fn(clsf, labels)
 
             loss.backward()
             self.opt.step()
@@ -187,12 +186,10 @@ class ResNetAgent(Agent):
                 images = batch["image"].to(self.device)
                 labels = batch["label"].to(self.device)
 
-                cls_soft = torch.softmax(
-                    self.model(images),
-                    dim=-1
-                )
+                clsf = self.model(images)
+                cls_soft = torch.softmax(clsf, dim=-1)
 
-                loss = self.loss_fn(cls_soft, labels)
+                loss = self.loss_fn(clsf, labels)
 
                 epoch_val_loss += loss
                 epoch_val_acc += self.__calc_accuracy__(cls_soft, labels)
@@ -216,16 +213,14 @@ class ResNetAgent(Agent):
                 images = batch["image"].to(self.device)
                 labels = batch["label"].to(self.device)
 
-                cls_soft = torch.softmax(
-                    self.model(images),
-                    dim=-1
-                )
+                clsf = self.model(images)
+                cls_soft = torch.softmax(clsf, dim=-1)
 
-                loss = self.loss_fn(cls_soft, labels)
+                loss = self.loss_fn(clsf, labels)
+
                 epoch_val_loss += loss
                 epoch_val_acc += self.__calc_accuracy__(cls_soft, labels)
 
-                # pass this to the cnf_mat() function
                 all_predictions = torch.cat((all_predictions, torch.argmax(cls_soft, dim=-1)))
                 all_labels = torch.cat((all_labels, labels))
 